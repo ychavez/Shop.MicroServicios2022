532927192d982fd62c0caf80f93bc7d45993ab7e
Yael Chavez <you400@gmail.com>
2022-02-26 11:30:30 -0600

    basket final

---
+++ Services/Basket.API/Basket.API/Repositories/Basketrepository.cs
@@ -0,0 +1,38 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class Basketrepository : IBasketRepository
+    {
+        private readonly IDistributedCache redisCache;
+
+        public Basketrepository(IDistributedCache redisCache)
+        {
+            this.redisCache = redisCache;
+        }
+        public async Task DeleteBasket(string userName)
+        {
+            await redisCache.RemoveAsync(userName);
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await redisCache.GetStringAsync(userName);
+            
+            if (string.IsNullOrEmpty(basket))
+                return null;
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await redisCache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+
+            return await GetBasket(basket.UserName);
+        }
+    }
+}
