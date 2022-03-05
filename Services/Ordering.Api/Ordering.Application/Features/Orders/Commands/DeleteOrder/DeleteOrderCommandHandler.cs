68ea3b8db284389db2eea7c041800ebbbb8b4b4a
Yael Chavez <you400@gmail.com>
2022-03-05 11:12:48 -0600

    delete command

---
+++ Services/Ordering.Api/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Ordering.Application.Contracts;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+    {
+        private readonly IAsyncRepository<Order> repository;
+
+        public DeleteOrderCommandHandler(IAsyncRepository<Order> repository)
+        {
+            this.repository = repository;
+        }
+
+
+        public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
