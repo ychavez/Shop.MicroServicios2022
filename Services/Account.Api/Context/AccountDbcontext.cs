54db8e4050c9ccae33078f942e828e6b5d05600c
Yael Chavez <you400@gmail.com>
2022-03-05 15:05:37 -0600

    account

---
+++ Services/Account.Api/Context/AccountDbcontext.cs
@@ -0,0 +1,13 @@
+﻿using Account.Api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Account.Api.Context
+{
+    public class AccountDbcontext: IdentityDbContext<ShopUser>
+    {
+        public AccountDbcontext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
